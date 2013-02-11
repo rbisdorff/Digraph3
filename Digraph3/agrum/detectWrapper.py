6740da02781d1bb6cf5f5745be4d343eb54925f2
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2013-02-11 13:46:19 +0000

    sync

--- Digraph3/agrum/detectWrapper.py
+++ Digraph3/agrum/detectWrapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env/python
+#!/usr/bin/env/python3
 
 def detectWrapper(graph,Debug=True):
     """
@@ -6,13 +6,13 @@ def detectWrapper(graph,Debug=True):
     """
     import os
     from tempfile import NamedTemporaryFile
-    fo = NamedTemporaryFile(delete=False)
+    fo = NamedTemporaryFile('w',delete=False)
     tempFileName = fo.name
     Med = graph.valuationdomain['med']
     actions = [x for x in graph.actions]
     #actions.sort()
     relation = graph.relation
-    na = range(len(actions))
+    na = list(range(len(actions)))
     for i in na:
         for j in na:
             if i != j:
@@ -21,22 +21,22 @@ def detectWrapper(graph,Debug=True):
     fo.close()
     resultFile = tempFileName+'.py'
     os.system('./detectChordlessCircuits ' + tempFileName + ' ' + resultFile)
-    execfile(str(resultFile))
+    exec(compile(open(str(resultFile)).read(), str(resultFile), 'exec'))
     if Debug:
-        print 'see file: ', tempFileName
-        print resultFile
-        print locals()['circuitsList']
+        print('see file: ', tempFileName)
+        print(resultFile)
+        print(locals()['circuitsList'])
     circuits = locals()['circuitsList']
     if circuits != []:
         Detected = True
         if Debug:
-            print 'A chordless circuit has been detected !'
-            print 'certificate: ', circuits
+            print('A chordless circuit has been detected !')
+            print('certificate: ', circuits)
     else:
         Detected = False
         if Debug:
-            print 'No chordless circuit has been detected !'
-            print 'certificate: ', circuits
+            print('No chordless circuit has been detected !')
+            print('certificate: ', circuits)
 
     return Detected
 
@@ -50,19 +50,19 @@ t0 = time()
 #g = BipolarOutrankingDigraph(t)
 #g.showRelationTable()
 g = RandomDigraph(order=200,arcProbability=0.03)
-print "Finished graph construction in ",time()-t0,'sec-'
+print("Finished graph construction in ",time()-t0,'sec-')
 t0 = time()
 if detectWrapper(g,Debug=False):
-    print 'Chordless Circuit(s) detected !'
+    print('Chordless Circuit(s) detected !')
 else:
-    print 'No Chordless Circuit detected !'
-print 'C++/Agrum wrapper time = ', time() - t0
+    print('No Chordless Circuit detected !')
+print('C++/Agrum wrapper time = ', time() - t0)
 t0 = time()
 if g.detectCppChordlessCircuits():
-    print 'Chordless Circuit(s) detected !'
+    print('Chordless Circuit(s) detected !')
 else:
-    print 'No Chordless Circuit detected !'
-print 'C++/Agrum wrapper time = ', time() - t0
+    print('No Chordless Circuit detected !')
+print('C++/Agrum wrapper time = ', time() - t0)
 ## t0 = time()
 ## if len(g.computeChordlessCircuits()) != 0:
 ##     print 'Chordless Circuit(s) detected !'
