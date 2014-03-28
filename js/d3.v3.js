4f44db1398da154f02acdaad60b3d5a6400cba19
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2014-03-28 15:22:24 +0000

    sync
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@562 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- js/d3.v3.js
+++ js/d3.v3.js
@@ -1,3 +1,42 @@
+/*
+Copyright (c) 2010-2014, Michael Bostock
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* The name Michael Bostock may not be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MICHAEL BOSTOCK BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*
+
+Additions made by Gary Cornelius, Student at the University of Luxembourg during my Bachelor Thesis.
+
+Additional features:
+    * Added possibility to add links by name i.e. {source:"link1",target:"link2"}
+
+
+*/
+
 !function() {
   var d3 = {
     version: "3.4.3"
@@ -6263,14 +6302,17 @@
       }
       for (i = 0; i < m; ++i) {
         o = links[i];
-        
-      	if(typeof o.source == "string" || typeof o.target =="string") {
+        /*
+        Changed lines. 
+        */
+      	if(typeof o.source == "string" || typeof o.target == "string") {
       		for (var k = 0; k < n; ++k) {
 				if (nodes[k].name === o.source) links[i].source = nodes[k].index;
        			if (nodes[k].name === o.target) links[i].target = nodes[k].index;
         	}
         }
-        
+        /*
+        */
         o = links[i];
         if (typeof o.source == "number") o.source = nodes[o.source];
         if (typeof o.target == "number") o.target = nodes[o.target];
