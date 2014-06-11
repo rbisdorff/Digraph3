7d8d697e4d2f4542b5638430aca611c812cab847
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2014-06-11 10:00:23 +0000

    sync
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@787 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

---
+++ docSphinx/tutRandValDigraph.py
@@ -0,0 +1,77 @@
+# automatically generated random irreflexive digraph
+from decimal import Decimal
+actionset = [
+'1',
+'2',
+'3',
+'4',
+'5',
+'6',
+'7',
+]
+valuationdomain = {'hasIntegerValuation': False, 'min': Decimal("-1.0"),'med': Decimal("0.0"),'max': Decimal("1.0")}
+relation = {
+'1': {
+'1': Decimal("0.0"),
+'2': Decimal("-0.480"),
+'3': Decimal("0.700"),
+'4': Decimal("0.860"),
+'5': Decimal("0.300"),
+'6': Decimal("0.380"),
+'7': Decimal("0.440"),
+},
+'2': {
+'1': Decimal("-0.220"),
+'2': Decimal("0.0"),
+'3': Decimal("-0.380"),
+'4': Decimal("0.500"),
+'5': Decimal("0.80"),
+'6': Decimal("-0.540"),
+'7': Decimal("0.020"),
+},
+'3': {
+'1': Decimal("-0.420"),
+'2': Decimal("0.080"),
+'3': Decimal("0.0"),
+'4': Decimal("0.700"),
+'5': Decimal("-0.560"),
+'6': Decimal("0.840"),
+'7': Decimal("-1.00"),
+},
+'4': {
+'1': Decimal("0.440"),
+'2': Decimal("-0.40"),
+'3': Decimal("-0.620"),
+'4': Decimal("0.0"),
+'5': Decimal("0.040"),
+'6': Decimal("0.660"),
+'7': Decimal("0.760"),
+},
+'5': {
+'1': Decimal("0.320"),
+'2': Decimal("-0.480"),
+'3': Decimal("-0.460"),
+'4': Decimal("0.640"),
+'5': Decimal("0.0"),
+'6': Decimal("-0.220"),
+'7': Decimal("-0.520"),
+},
+'6': {
+'1': Decimal("-0.840"),
+'2': Decimal("0.00"),
+'3': Decimal("-0.40"),
+'4': Decimal("-0.960"),
+'5': Decimal("-0.180"),
+'6': Decimal("0.0"),
+'7': Decimal("-0.220"),
+},
+'7': {
+'1': Decimal("0.880"),
+'2': Decimal("0.720"),
+'3': Decimal("0.820"),
+'4': Decimal("0.520"),
+'5': Decimal("-0.840"),
+'6': Decimal("0.040"),
+'7': Decimal("0.0"),
+},
+}
