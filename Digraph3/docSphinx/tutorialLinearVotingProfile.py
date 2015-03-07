9e5e0f60a60824d4afcc9b696398666919e8064a
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2015-03-07 08:48:38 +0000

    sync

---
+++ Digraph3/docSphinx/tutorialLinearVotingProfile.py
@@ -0,0 +1,45 @@
+# Saved linear voting profile: 
+candidates = {
+'a2': {'name': 'a2'},
+'a3': {'name': 'a3'},
+'a1': {'name': 'a1'},
+}
+voters = {
+'v4': {
+'weight':1.0},
+'v3': {
+'weight':1.0},
+'v1': {
+'weight':1.0},
+'v5': {
+'weight':1.0},
+'v2': {
+'weight':1.0},
+}
+linearBallot = {
+'v4': [
+'a1',
+'a2',
+'a3',
+],
+'v3': [
+'a1',
+'a3',
+'a2',
+],
+'v1': [
+'a2',
+'a1',
+'a3',
+],
+'v5': [
+'a3',
+'a1',
+'a2',
+],
+'v2': [
+'a3',
+'a1',
+'a2',
+],
+}
