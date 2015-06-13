e37a18b55c85f7e64540e9bf94f24e83a8f93c0e
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2015-06-13 13:21:32 +0000

    sync
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@1359 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- bigOutrankingDigraphs.py
+++ bigOutrankingDigraphs.py
@@ -24,65 +24,65 @@ from sortingDigraphs import *
 from time import time
 from decimal import Decimal
 
-def total_size(o, handlers={}, verbose=False):
-    """ Returns the approximate memory footprint of an object and all of its contents.
-
-    Automatically finds the contents of the following containers and
-    their subclasses:  tuple, list, deque, dict, set, frozenset, Digraph and BigDigraph.
-    To search other containers, add handlers to iterate over their contents:
-
-        handlers = {SomeContainerClass: iter,
-                    OtherContainerClass: OtherContainerClass.get_elements}
-
-    See http://code.activestate.com/recipes/577504/  
-
-    """
-    from sys import getsizeof, stderr
-    from itertools import chain
-    from collections import deque
-    
-    try:
-        from reprlib import repr
-    except ImportError:
-        pass
-
-    # built-in containers and their subclasses
-    dict_handler = lambda d: chain.from_iterable(d.items())
-    all_handlers = {tuple: iter,
-                    list: iter,
-                    deque: iter,
-                    dict: dict_handler,
-                    set: iter,
-                    frozenset: iter,
-                    }
-
-    # Digraph3 objects 
-    object_handler = lambda d: chain.from_iterable(d.__dict__.items())    
-    handlers = {BigDigraph: object_handler,
-                Digraph: object_handler,
-                PerformanceTableau : object_handler,
-                }
-    
-    all_handlers.update(handlers)     # user handlers take precedence
-    seen = set()                      # track which object id's have already been seen
-    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
-
-    def sizeof(o):
-        if id(o) in seen:       # do not double count the same object
-            return 0
-        seen.add(id(o))
-        s = getsizeof(o, default_size)
-
-        if verbose:
-            print(s, type(o), repr(o), file=stderr)
-
-        for typ, handler in all_handlers.items():
-            if isinstance(o, typ):
-                s += sum(map(sizeof, handler(o)))
-                break
-        return s
-
-    return sizeof(o)
+##def total_size(o, handlers={}, verbose=False):
+##    """ Returns the approximate memory footprint of an object and all of its contents.
+##
+##    Automatically finds the contents of the following containers and
+##    their subclasses:  tuple, list, deque, dict, set, frozenset, Digraph and BigDigraph.
+##    To search other containers, add handlers to iterate over their contents:
+##
+##        handlers = {SomeContainerClass: iter,
+##                    OtherContainerClass: OtherContainerClass.get_elements}
+##
+##    See http://code.activestate.com/recipes/577504/  
+##
+##    """
+##    from sys import getsizeof, stderr
+##    from itertools import chain
+##    from collections import deque
+##    
+##    try:
+##        from reprlib import repr
+##    except ImportError:
+##        pass
+##
+##    # built-in containers and their subclasses
+##    dict_handler = lambda d: chain.from_iterable(d.items())
+##    all_handlers = {tuple: iter,
+##                    list: iter,
+##                    deque: iter,
+##                    dict: dict_handler,
+##                    set: iter,
+##                    frozenset: iter,
+##                    }
+##
+##    # Digraph3 objects 
+##    object_handler = lambda d: chain.from_iterable(d.__dict__.items())    
+##    handlers = {BigDigraph: object_handler,
+##                Digraph: object_handler,
+##                PerformanceTableau : object_handler,
+##                }
+##    
+##    all_handlers.update(handlers)     # user handlers take precedence
+##    seen = set()                      # track which object id's have already been seen
+##    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+##
+##    def sizeof(o):
+##        if id(o) in seen:       # do not double count the same object
+##            return 0
+##        seen.add(id(o))
+##        s = getsizeof(o, default_size)
+##
+##        if verbose:
+##            print(s, type(o), repr(o), file=stderr)
+##
+##        for typ, handler in all_handlers.items():
+##            if isinstance(o, typ):
+##                s += sum(map(sizeof, handler(o)))
+##                break
+##        return s
+##
+##    return sizeof(o)
 
 class BigDigraph(object):
     """
