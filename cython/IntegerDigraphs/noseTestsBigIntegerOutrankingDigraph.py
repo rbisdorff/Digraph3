2b23e6d87ff78ffdc80599a448f89d790a8cceed
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2016-07-15 01:14:09 +0000

    reducing memory requirements
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@1869 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- cython/IntegerDigraphs/noseTestsBigIntegerOutrankingDigraph.py
+++ cython/IntegerDigraphs/noseTestsBigIntegerOutrankingDigraph.py
@@ -14,82 +14,82 @@ from cRandPerfTabs import Random3ObjectivesPerformanceTableau as cR3ObjPT
 #from outrankingDigraphs import BipolarOutrankingDigraph
 from time import time
 
-## def testbigOutrankingDigraph():
-##     print('==>> Testing bigOutrankingDigraph instantiation')
-##     #from outrankingDigraphs import BipolarOutrankingDigraph
-##     MP = True
-##     t0 = time()
-##     ctp = cR3ObjPT(numberOfActions=100,seed=100)
-##     #tp = R3ObjPT(numberOfActions=100,seed=100)
-##     print(time()-t0)
-##     print(total_size(ctp.evaluation))
-##     bg1 = BigIntegerOutrankingDigraph(ctp,quantiles=10,quantilesOrderingStrategy='average',
-##                                 LowerClosed=True,
-##                                 CopyPerfTab=True,
-##                                minimalComponentSize=1,
-##                                     Threading=MP,Debug=False)
-##     print(bg1.computeDecompositionSummaryStatistics())
-##     bg1.showDecomposition()
-##     print(bg1)
-##     t0 = time()
-##     gi = IntegerBipolarOutrankingDigraph(ctp,Threading=MP)
-##     #g = BipolarOutrankingDigraph(tp,Normalized=True,Threading=MP)
-##     print(time()-t0)
-##     print(total_size(gi))
-##     t0 = time()
-##     print("Big outranking digraph's correlation with standard outranking digraph")
-##     print(bg1.computeOrdinalCorrelation(gi,Debug=False))
-##     print(time()-t0)
+def testbigOutrankingDigraph():
+    print('==>> Testing bigOutrankingDigraph instantiation')
+    #from outrankingDigraphs import BipolarOutrankingDigraph
+    MP = True
+    t0 = time()
+    ctp = cR3ObjPT(numberOfActions=100,seed=100)
+    #tp = R3ObjPT(numberOfActions=100,seed=100)
+    print(time()-t0)
+    print(total_size(ctp.evaluation))
+    bg1 = BigIntegerOutrankingDigraph(ctp,quantiles=10,quantilesOrderingStrategy='average',
+                                LowerClosed=True,
+                                CopyPerfTab=True,
+                               minimalComponentSize=1,
+                                    Threading=MP,Debug=False)
+    print(bg1.computeDecompositionSummaryStatistics())
+    bg1.showDecomposition()
+    print(bg1)
+    t0 = time()
+    gi = IntegerBipolarOutrankingDigraph(ctp,Threading=MP)
+    #g = BipolarOutrankingDigraph(tp,Normalized=True,Threading=MP)
+    print(time()-t0)
+    print(total_size(gi))
+    t0 = time()
+    print("Big outranking digraph's correlation with standard outranking digraph")
+    print(bg1.computeOrdinalCorrelation(gi,Debug=False))
+    print(time()-t0)
 
-## def testMinimalComponentSize():
-##     print('==>> Testing bigOutrankingDigraph with minimal Component Size instantiation')
-##     MP = True
-##     t0 = time()
-##     tp = RandomCBPerformanceTableau(numberOfActions=200,Threading=MP,seed=None)
-##     print(time()-t0)
-##     print(total_size(tp.evaluation))
-##     bg1 = BigIntegerOutrankingDigraph(tp,quantiles=5,quantilesOrderingStrategy='average',
-##                                 LowerClosed=True,
-##                                 CopyPerfTab=True,
-##                                minimalComponentSize=5,
-##                                     Threading=MP,Debug=False)
-##     print(bg1.computeDecompositionSummaryStatistics())
-##     bg1.showDecomposition()
-##     print(bg1)
-##     bg1.showRelationTable()
-##     tp = RandomPerformanceTableau(numberOfActions=200,seed=None)
-##     #tp = RandomCBPerformanceTableau(numberOfActions=200,Threading=MP,seed=None)
-##     #tp = Random3ObjectivesPerformanceTableau(numberOfActions=200,seed=None)
-##     bg2 = BigIntegerOutrankingDigraph(tp,quantiles=35,quantilesOrderingStrategy='average',
-##                                 LowerClosed=False,
-##                                minimalComponentSize=20,
-##                                     Threading=MP,Debug=False)
-##     print(bg2.computeDecompositionSummaryStatistics())
-##     bg2.showDecomposition(direction="increasing")
-##     bg2.showDecomposition()
-##     #bg2.showRelationTable()
-##     print(bg1.computeOrdinalCorrelation(bg2,Debug=False))
-##     #bg1.recodeValuation(-1,1,Debug=True)
-##     print(bg2.computeOrdinalCorrelation(bg2,Debug=False))
-##     print(bg2)
-##     bg2.showRelationMap(0,50)
+def testMinimalComponentSize():
+    print('==>> Testing bigOutrankingDigraph with minimal Component Size instantiation')
+    MP = True
+    t0 = time()
+    tp = RandomCBPerformanceTableau(numberOfActions=200,Threading=MP,seed=None)
+    print(time()-t0)
+    print(total_size(tp.evaluation))
+    bg1 = BigIntegerOutrankingDigraph(tp,quantiles=5,quantilesOrderingStrategy='average',
+                                LowerClosed=True,
+                                CopyPerfTab=True,
+                               minimalComponentSize=5,
+                                    Threading=MP,Debug=False)
+    print(bg1.computeDecompositionSummaryStatistics())
+    bg1.showDecomposition()
+    print(bg1)
+    bg1.showRelationTable()
+    tp = RandomPerformanceTableau(numberOfActions=200,seed=None)
+    #tp = RandomCBPerformanceTableau(numberOfActions=200,Threading=MP,seed=None)
+    #tp = Random3ObjectivesPerformanceTableau(numberOfActions=200,seed=None)
+    bg2 = BigIntegerOutrankingDigraph(tp,quantiles=35,quantilesOrderingStrategy='average',
+                                LowerClosed=False,
+                               minimalComponentSize=20,
+                                    Threading=MP,Debug=False)
+    print(bg2.computeDecompositionSummaryStatistics())
+    bg2.showDecomposition(direction="increasing")
+    bg2.showDecomposition()
+    #bg2.showRelationTable()
+    print(bg1.computeOrdinalCorrelation(bg2,Debug=False))
+    #bg1.recodeValuation(-1,1,Debug=True)
+    print(bg2.computeOrdinalCorrelation(bg2,Debug=False))
+    print(bg2)
+    bg2.showRelationMap(0,50)
 
-## def testBestChoiceRecommendation():
-##     print('==>> Testing ordinal correlations')
-##     MP = True
-##     tp = RandomCBPerformanceTableau(numberOfActions=200,Threading=MP,seed=None)
-##     bg2 = BigIntegerOutrankingDigraph(tp,quantiles=35,quantilesOrderingStrategy='average',
-##                                 LowerClosed=False,
-##                                minimalComponentSize=10,
-##                                     Threading=MP,Debug=False)
-##     t0=time();print(bg2.computeDeterminateness());print(time()-t0,'sec.')
-##     #bg2.showHTMLRelationMap(0,0,Colored=True)
-##     bg2.showBestChoiceRecommendation(Comments=False)
+def testBestChoiceRecommendation():
+    print('==>> Testing ordinal correlations')
+    MP = True
+    tp = RandomCBPerformanceTableau(numberOfActions=200,Threading=MP,seed=None)
+    bg2 = BigIntegerOutrankingDigraph(tp,quantiles=35,quantilesOrderingStrategy='average',
+                                LowerClosed=False,
+                               minimalComponentSize=10,
+                                    Threading=MP,Debug=False)
+    t0=time();print(bg2.computeDeterminateness());print(time()-t0,'sec.')
+    #bg2.showHTMLRelationMap(0,0,Colored=True)
+    bg2.showBestChoiceRecommendation(Comments=False)
 
 def testSparseModelFitness():
     print('==>> Testing sparse modeling fitness')
     MP = True
-    Nsim = 100
+    Nsim = 2
     nbrOfActions = 250
     nbrOfCriteria = 13
     qTiles = 25
