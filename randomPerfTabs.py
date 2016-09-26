212267091b423a492aefb225e48d3917b15e0ae0
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2016-09-26 11:53:22 +0000

    sync
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@1945 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- randomPerfTabs.py
+++ randomPerfTabs.py
@@ -2293,11 +2293,9 @@ class RandomCBPerformanceGenerator(object):
        
     def randomAction(self):
         """
-        Returns ``{ 'action': { 'key': actionKey, 'shortName': ..., 'name': ..., ``
-         ``                   'type': 'neutral'|'advantageous'|'cheap'}, ``
-         ``   'evaluation': {'g1': Decimal(...), 'g2': Decimal(...), ... }``
-         ``}``
-            
+        Returns :code:`{ 'action': { 'key': actionKey, 'shortName': ..., 'name': ...,  
+                                         'type': 'neutral'|'advantageous'|'cheap'},
+                        'evaluation': {'g1': Decimal(...), 'g2': Decimal(...), ... }}` .
         """
         # generate action key
         self.counter += 1
