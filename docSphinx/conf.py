9cb34a87f4169c70beddabba47021bc9de20d644
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2018-07-26 14:32:38 +0000

    Added CSVPerformanceTableau class
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@2667 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- docSphinx/conf.py
+++ docSphinx/conf.py
@@ -255,5 +255,5 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('https://docs.python.org/3.5', None)}
+intersphinx_mapping = {'python': ('https://docs.python.org/3.7', None)}
 #{'http://docs.python.org/': None}
