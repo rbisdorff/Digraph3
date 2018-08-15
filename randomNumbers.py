a768404ca9631403e083dae5b55567bcd653d673
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2018-08-15 12:18:38 +0000

    Added a constructor for quasi random Korobov point sets
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@2681 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- randomNumbers.py
+++ randomNumbers.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-# Python3+ implementation of random number generators
-# Copyright (C) 2014  Raymond Bisdorff
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
+"""
+Python3+ implementation of random number generators
+Copyright (C) 2014-2018  Raymond Bisdorff
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
 #######################
 
 __version__ = "Branch: 3.6 $"
