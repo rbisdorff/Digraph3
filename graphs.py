0b6aac373a9241eee6889b4ec1f9b58e89640a5b
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2019-02-04 14:40:55 +0000

    Adding a permutation graph constructor to the graphs module
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@2917 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- graphs.py
+++ graphs.py
@@ -18,7 +18,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-    See :ref:'_Graphs-Tutorial-label`.
+    See :ref:'Graphs-Tutorial-label`.
 """
 #############################################
 from decimal import Decimal
