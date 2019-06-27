29e9cb29fcc17221c30bb530277853f7713369b9
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2019-06-27 13:17:07 +0000

    Added a tutorial on ordinal correlations
    git-svn-id: http://leopold-loewenheim.uni.lu/svn/repos/Digraph3@3237 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- outrankingDigraphs.py
+++ outrankingDigraphs.py
@@ -997,130 +997,155 @@ class OutrankingDigraph(Digraph,PerformanceTableau):
     def export3DplotOfCriteriaCorrelation(self,plotFileName="correlation",
                                           ValuedCorrelation=False,Type="pdf",
                                           Comments=False,bipolarFlag=False,
-                                          dist=True,centeredFlag=False):
+                                          dist=True,centeredFlag=False,
+                                          tempDir=None):
         """
         use Calmat and R for producing a plot of the principal components of
         the criteria ordinal correlation table.
+
+        *Parameters*;
+
+            * *plotFileName* := name of the created R plot image.
+            * *Type* := format of graphics file: pdf, png, jpeg, xfig 
+            * *ValuedCorrelation* := False (tau by default) | True (r(<=>) otherwise
+            * *tempDir* : if None the tempfile.gettempdir() is used to get the platform independent temporary directory.
+            * *bipolarFlag*: obsolete
+            * *dist*: internal Calmat flag
+            * *centeredFlag* : internal Calmat flag
+            
         """
         import time
-        
-        if Comments:
-            print('*----  export 3dplot of type %s -----' % (Type))
+        from tempfile import TemporaryDirectory, gettempdir
         import os
-        criteriaList = [x for x in self.criteria]
-        criteriaList.sort()
-        n = len(criteriaList)
-        fo = open('criteriaLabels.prn','w')
-        for key in criteriaList:
-            fo.write('%s ' % (key))
-        fo.close()
-        self.saveCriteriaCorrelationTable(fileName='tempcorr.prn',Silent=True,
-                                          ValuedCorrelation=ValuedCorrelation,
-                                          Bipolar=bipolarFlag,Centered=centeredFlag)
-        # create Calmat script and calmat execution (the prn extension is standard)
-        try:
+        currentDir = os.getcwd()
+        #print(currentDir)
+        plotFileName = '%s/%s' % (currentDir,plotFileName)
+        if Comments:
+            print('3DplotFileName: %s' % plotFileName)
+        if tempDir == None:
+            tempDir = gettempdir()
+        with TemporaryDirectory(dir=tempDir) as tempDirName:
             if Comments:
-                if dist:
-                    cmd = 'env defdist.sh tempcorr '+str(n)+' '+str(n)
-                else:
-                    cmd = 'env defdista.sh tempcorr '+str(n)+' '+str(n) 
-            else:
-                if dist:
-                    cmd = 'env defdist.sh tempcorr '+str(n)+' '+str(n)+' > /dev/null'
-                else:
-                    cmd = 'env defdista.sh tempcorr '+str(n)+' '+str(n)+' > /dev/null'
-            os.system(cmd)
-            if Comments:
-                os.system('env calmat tempcorr.prg')
-            else:
-                os.system('env calmat tempcorr.prg > /dev/null')
-        except:
-            print('Error: You need to install calmat !!!')
-            return
-        # create R 3d scatter plot script
-        if Type == "interactive":
-            fo = open('scatter.r','w')
-            fo.write('# 3d scatter plot RB April 2008\n')
-            fo.write('test.mat <- matrix(scan("compolg.prn"),ncol=%d,byrow=T)\n' % (n))
-##             fo.write('choose<-c(%d,%d,%d)\n' % (1,2,3))
-            fo.write('choose<-c(%d,%d,%d)\n' % (n,n-1,n-2))
-            fo.write('test.labels <- scan("criteriaLabels.prn",what="character")\n') 
-            fo.write('valprop <- matrix(scan("val_prlg.prn"),ncol=2,byrow=T)\n') 
-            fo.write('require(rgl)\n')
-            fo.write('open3d()\n')
-            fo.write('points3d(test.mat[,choose])\n')
-            fo.write('text3d(test.mat[,choose],text=test.labels,col="red3")\n')
-            fo.write('axes3d(edges=c("x","y","z"),pos=c(0,0,0),labels=F,ticks=F)\n')
-            fo.write('axes3d(edges=c("x","y","z"))\n')
-            fo.write('title3d(main="Criteria Ordinal Correlation",xlab=valprop[choose,2][1],ylab=valprop[choose,2][2],zlab=valprop[choose,2][3],col="blue",line=4)\n')
-            fo.write('rgl.viewpoint(1,1/4,interactive=T)\n')
-            fo.write('rgl.snapshot("%s.png")\n' % (plotFileName+'.png') )
+                print('*----  export 3dplot of type %s -----' % (Type))
+            os.chdir(tempDirName)
+            criteriaList = [x for x in self.criteria]
+            criteriaList.sort()
+            n = len(criteriaList)
+            fo = open('criteriaLabels.prn','w')
+            for key in criteriaList:
+                fo.write('%s ' % (key))
             fo.close()
-        else:
-            fo = open('scatter.r','w')
-            fo.write('# scatter plot RB April 2008\n')
-            fo.write('test.mat <- matrix(scan("compolg.prn"),ncol=%d,byrow=T)\n' % (n))
-            fo.write('lim1 <- max(test.mat)\n')
-            fo.write('lim2 <- min(test.mat)\n')
-            if centeredFlag:
-                fo.write('choose12<-c(%d,%d)\n' % (1,2))
-                fo.write('choose23<-c(%d,%d)\n' % (2,3))
-                fo.write('choose13<-c(%d,%d)\n' % (1,3))
-                fo.write('choose21<-c(%d,%d)\n' % (2,1))
+            self.saveCriteriaCorrelationTable(fileName='tempcorr.prn',Silent=True,
+                                              ValuedCorrelation=ValuedCorrelation,
+                                              Bipolar=bipolarFlag,Centered=centeredFlag)
+            # create Calmat script and calmat execution (the prn extension is standard)
+            try:
+                if Comments:
+                    if dist:
+                        cmd = 'env defdist.sh tempcorr '+str(n)+' '+str(n)
+                    else:
+                        cmd = 'env defdista.sh tempcorr '+str(n)+' '+str(n) 
+                else:
+                    if dist:
+                        cmd = 'env defdist.sh tempcorr '+str(n)+' '+str(n)+' > /dev/null'
+                    else:
+                        cmd = 'env defdista.sh tempcorr '+str(n)+' '+str(n)+' > /dev/null'
+                os.system(cmd)
+                if Comments:
+                    os.system('env calmat tempcorr.prg')
+                else:
+                    os.system('env calmat tempcorr.prg > /dev/null')
+            except:
+                print('Error: You need to install calmat !!!')
+                return
+            # create R 3d scatter plot script
+            if Type == "interactive":
+                fo = open('scatter.r','w')
+                fo.write('# 3d scatter plot RB April 2008\n')
+                fo.write('test.mat <- matrix(scan("compolg.prn"),ncol=%d,byrow=T)\n' % (n))
+    ##             fo.write('choose<-c(%d,%d,%d)\n' % (1,2,3))
+                fo.write('choose<-c(%d,%d,%d)\n' % (n,n-1,n-2))
+                fo.write('test.labels <- scan("criteriaLabels.prn",what="character")\n') 
+                fo.write('valprop <- matrix(scan("val_prlg.prn"),ncol=2,byrow=T)\n') 
+                fo.write('require(rgl)\n')
+                fo.write('open3d()\n')
+                fo.write('points3d(test.mat[,choose])\n')
+                fo.write('text3d(test.mat[,choose],text=test.labels,col="red3")\n')
+                fo.write('axes3d(edges=c("x","y","z"),pos=c(0,0,0),labels=F,ticks=F)\n')
+                fo.write('axes3d(edges=c("x","y","z"))\n')
+                fo.write('title3d(main="Criteria Ordinal Correlation",xlab=valprop[choose,2][1],ylab=valprop[choose,2][2],zlab=valprop[choose,2][3],col="blue",line=4)\n')
+                fo.write('rgl.viewpoint(1,1/4,interactive=T)\n')
+                fo.write('rgl.snapshot("%s.png")\n' % (plotFileName+'.png') )
+                fo.close()
             else:
-                fo.write('choose12<-c(%d,%d)\n' % (n,n-1))
-                fo.write('choose23<-c(%d,%d)\n' % (n-1,n-2))
-                fo.write('choose13<-c(%d,%d)\n' % (n,n-2))
-                fo.write('choose21<-c(%d,%d)\n' % (n-1,n))
-            fo.write('test.labels <- scan("criteriaLabels.prn",what="character")\n') 
-            fo.write('valprop <- matrix(scan("val_prlg.prn"),ncol=2,byrow=T)\n')
-            if Type == "png":
-                fo.write('png("%s.png",width=480,height=480,bg="cornsilk")\n' % (plotFileName) )
-            elif Type == "jpeg":
-                fo.write('jpeg("%s.jpg",width=480,height=480,bg="cornsilk")\n' % (plotFileName) )
-            elif Type == "xfig":
-                fo.write('xfig("%s.fig",width=480,height=480,bg="cornsilk")\n' % (plotFileName) )
-            elif Type == "pdf":
-                fo.write('pdf("%s.pdf",width=6,height=6,bg="cornsilk",title="PCA of Criteria Correlation Index")\n' % (plotFileName) )
-            else:
-                print('Error: Plotting device %s not defined !' % (Type))
-                return     
-            fo.write('par(mfrow=c(2,2))\n')
-            fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose12,2][1]*100,"%"),ylab=paste("axis 2:",valprop[choose12,2][2]*100,"%"),type="n",asp=1)\n')
-            fo.write('abline(h=0,v=0,col="grey",lty="dotted")\n')            
-            fo.write('title(sub=paste("total inertia:",(valprop[choose12,2][1]+valprop[choose12,2][2])*100,"%"),main="factors 1 and 2",col="blue")\n')
-            fo.write('text(test.mat[,choose12],labels=test.labels,col="red3",cex=1.0)\n')
-##             fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose12,2][1]*100,"%"),ylab=paste("axis 2:",valprop[choose12,2][2]*100,"%"),type="n",asp=1)\n')
-            fo.write('plot(test.mat[,choose12],xlab=paste("axis 2:",valprop[choose23,2][1]*100,"%"),ylab=paste("axis 3:",valprop[choose23,2][2]*100,"%"),type="n",asp=1)\n')
-            fo.write('abline(h=0,v=0,col="grey",lty="dotted")\n')            
-            fo.write('title(sub=paste("total inertia:",(valprop[choose23,2][1]+valprop[choose23,2][2])*100,"%"),main="factors 2 and 3",col="blue")\n')
-            fo.write('text(test.mat[,choose23],labels=test.labels,col="red3",cex=1.0)\n')
-##             fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose12,2][1]*100,"%"),ylab=paste("axis 2:",valprop[choose12,2][2]*100,"%"),type="n",asp=1)\n')
-            fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose13,2][1]*100,"%"),ylab=paste("axis 3:",valprop[choose13,2][2]*100,"%"),type="n",asp=1)\n')
-            fo.write('abline(h=0,v=0,col="grey",lty="dotted")\n')            
-            fo.write('title(sub=paste("total inertia:",(valprop[choose13,2][1]+valprop[choose13,2][2])*100,"%"),main="factors 1 and 3",col="blue")\n')
-            fo.write('text(test.mat[,choose13],labels=test.labels,col="red3",cex=1.0)\n')
-            if centeredFlag:
-                fo.write('barplot(valprop[1:nrow(valprop)-1,2]*100,names.arg=c(1:%d),main="Axis inertia (in %%)",col="orangered")\n' % (n-1))
-            else:
-                fo.write('barplot(valprop[nrow(valprop):2,2]*100,names.arg=c(1:%d),main="Axis inertia (in %%)",col="orangered")\n' % (n-1))     
-            fo.write('dev.off()\n')           
-            fo.close()
-            
-        try:
-            ## if Comments:
-            ##     os.system('/usr/bin/R -q --vanilla --verbose < scatter.r 2>&1')
-            ## else:
-            ##     os.system('/usr/bin/R -q --vanilla < scatter.r > /dev/null 2> /dev/null')
-            if Comments:
-                os.system('env R -q --vanilla --verbose < scatter.r 2>&1')
-            else:
-                os.system('env R -q --vanilla < scatter.r > /dev/null 2> /dev/null')
-            time.sleep(3)     
-            if Comments:
-                print('See %s.%s ! ' % (plotFileName,Type))
-        except:
-            print('Error: You need to install R !!!')
+                fo = open('scatter.r','w')
+                fo.write('# scatter plot RB April 2008\n')
+                fo.write('test.mat <- matrix(scan("compolg.prn"),ncol=%d,byrow=T)\n' % (n))
+                fo.write('lim1 <- max(test.mat)\n')
+                fo.write('lim2 <- min(test.mat)\n')
+                if centeredFlag:
+                    fo.write('choose12<-c(%d,%d)\n' % (1,2))
+                    fo.write('choose23<-c(%d,%d)\n' % (2,3))
+                    fo.write('choose13<-c(%d,%d)\n' % (1,3))
+                    fo.write('choose21<-c(%d,%d)\n' % (2,1))
+                else:
+                    fo.write('choose12<-c(%d,%d)\n' % (n,n-1))
+                    fo.write('choose23<-c(%d,%d)\n' % (n-1,n-2))
+                    fo.write('choose13<-c(%d,%d)\n' % (n,n-2))
+                    fo.write('choose21<-c(%d,%d)\n' % (n-1,n))
+                fo.write('test.labels <- scan("criteriaLabels.prn",what="character")\n') 
+                fo.write('valprop <- matrix(scan("val_prlg.prn"),ncol=2,byrow=T)\n')
+                if Type == "png":
+                    fo.write('png("%s.png",width=480,height=480,bg="cornsilk")\n' % (plotFileName) )
+                elif Type == "jpeg":
+                    fo.write('jpeg("%s.jpg",width=480,height=480,bg="cornsilk")\n' % (plotFileName) )
+                elif Type == "xfig":
+                    fo.write('xfig("%s.fig",width=480,height=480,bg="cornsilk")\n' % (plotFileName) )
+                elif Type == "pdf":
+                    fo.write('pdf("%s.pdf",width=6,height=6,bg="cornsilk",title="PCA of Criteria Correlation Index")\n' % (plotFileName) )
+                else:
+                    print('Error: Plotting device %s not defined !' % (Type))
+                    return     
+                fo.write('par(mfrow=c(2,2))\n')
+                fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose12,2][1]*100,"%"),ylab=paste("axis 2:",valprop[choose12,2][2]*100,"%"),type="n",asp=1)\n')
+                fo.write('abline(h=0,v=0,col="grey",lty="dotted")\n')            
+                fo.write('title(sub=paste("total inertia:",(valprop[choose12,2][1]+valprop[choose12,2][2])*100,"%"),main="factors 1 and 2",col="blue")\n')
+                fo.write('text(test.mat[,choose12],labels=test.labels,col="red3",cex=1.0)\n')
+    ##             fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose12,2][1]*100,"%"),ylab=paste("axis 2:",valprop[choose12,2][2]*100,"%"),type="n",asp=1)\n')
+                fo.write('plot(test.mat[,choose12],xlab=paste("axis 2:",valprop[choose23,2][1]*100,"%"),ylab=paste("axis 3:",valprop[choose23,2][2]*100,"%"),type="n",asp=1)\n')
+                fo.write('abline(h=0,v=0,col="grey",lty="dotted")\n')            
+                fo.write('title(sub=paste("total inertia:",(valprop[choose23,2][1]+valprop[choose23,2][2])*100,"%"),main="factors 2 and 3",col="blue")\n')
+                fo.write('text(test.mat[,choose23],labels=test.labels,col="red3",cex=1.0)\n')
+    ##             fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose12,2][1]*100,"%"),ylab=paste("axis 2:",valprop[choose12,2][2]*100,"%"),type="n",asp=1)\n')
+                fo.write('plot(test.mat[,choose12],xlab=paste("axis 1:",valprop[choose13,2][1]*100,"%"),ylab=paste("axis 3:",valprop[choose13,2][2]*100,"%"),type="n",asp=1)\n')
+                fo.write('abline(h=0,v=0,col="grey",lty="dotted")\n')            
+                fo.write('title(sub=paste("total inertia:",(valprop[choose13,2][1]+valprop[choose13,2][2])*100,"%"),main="factors 1 and 3",col="blue")\n')
+                fo.write('text(test.mat[,choose13],labels=test.labels,col="red3",cex=1.0)\n')
+                if centeredFlag:
+                    fo.write('barplot(valprop[1:nrow(valprop)-1,2]*100,names.arg=c(1:%d),main="Axis inertia (in %%)",col="orangered")\n' % (n-1))
+                else:
+                    fo.write('barplot(valprop[nrow(valprop):2,2]*100,names.arg=c(1:%d),main="Axis inertia (in %%)",col="orangered")\n' % (n-1))     
+                fo.write('dev.off()\n')           
+                fo.close()
+                
+            try:
+                ## if Comments:
+                ##     os.system('/usr/bin/R -q --vanilla --verbose < scatter.r 2>&1')
+                ## else:
+                ##     os.system('/usr/bin/R -q --vanilla < scatter.r > /dev/null 2> /dev/null')
+                if Comments:
+                    os.system('env R -q --vanilla --verbose < scatter.r 2>&1')
+                else:
+                    os.system('env R -q --vanilla < scatter.r > /dev/null 2> /dev/null')
+                time.sleep(3)     
+                if Comments:
+                    print('See %s.%s ! ' % (plotFileName,Type))
+            except:
+                print('Error: You need to install R !!!')
+
+        # reset current working dir !!!
+        os.chdir(currentDir)
+        
 
     def export3DplotOfActionsCorrelation(self,plotFileName="correlation",Type="pdf",Comments=False,bipolarFlag=False,dist=True,centeredFlag=False):
         """
@@ -1128,7 +1153,9 @@ class OutrankingDigraph(Digraph,PerformanceTableau):
         the the actions ordinal correlation table.
         """
         import time
-        
+        #from tempfile import TemporaryDirectory
+        #with TemporaryDirectory(dir=tempDir) as tempDirName:
+    
         if Comments:
             print('*----  export 3dplot of type %s -----' % (Type))
         import os
