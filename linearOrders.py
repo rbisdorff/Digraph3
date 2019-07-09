221db13a8ac2c9ce412597392e100f6adacf28a5
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2019-07-09 10:33:59 +0000

    Added tutorial on valued kernel membership computation
    git-svn-id: https://leopold-loewenheim.uni.lu/svn/repos/Digraph3@3265 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- linearOrders.py
+++ linearOrders.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Python implementation of linear orders
-# Dependancy: digraphs 1.589+
-# Current revision $Revision: 1.18 $
-# Copyright (C) 2011  Raymond Bisdorff
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
+"""
+Python implementation of linear orders
+Dependancy: digraphs module
+Copyright (C) 2011-2019  Raymond Bisdorff
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
 #######################
 
 __version__ = "$Revision: 1.18 $"
--- weakOrders.py
+++ weakOrders.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-# Python3+ implementation of digraphs
-# Based on Python 2 $Revision: 1.697 $
-# Copyright (C) 2006-2014  Raymond Bisdorff
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
+"""
+Python3+ implementation of digraphs
+Module for working with weak orders
+Copyright (C) 2006-2019  Raymond Bisdorff
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
 #######################
 
 __version__ = "Branch: 3.3 $"
