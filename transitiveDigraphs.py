e140756b34e118796f468f24b74c59aa7e931232
bisdorff <bisdorff@1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc>
2020-02-27 17:03:58 +0000

    reviewing the votingProfiles module
    git-svn-id: https://leopold-loewenheim.uni.lu/svn/repos/Digraph3@3527 1edaa99d-39e4-4cdb-8da2-0a4aeb6909dc

--- transitiveDigraphs.py
+++ transitiveDigraphs.py
@@ -9,6 +9,7 @@ Copyright (C) 2006-2019  Raymond Bisdorff
 
     You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+    RB
 """
 #######################
 
