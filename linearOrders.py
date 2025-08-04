2cb5d11435447d777c8475ef31f3cc2e7d01e712
Raymond Bisdorff <raymond.bisdorff@pt.lu>
2025-08-04 17:05:36 +0200

    Added a new Bachet ranking algorithm with smart sampling of the permutohedron of decision actions

--- linearOrders.py
+++ linearOrders.py
@@ -1603,9 +1603,9 @@ class PolarisedBachetOrder(PolarisedBachetRanking):
     """
 #------------
 
-class RandomizedBachetRanking(PolarisedBachetRanking):
+class SmartBachetRanking(PolarisedBachetRanking):
     """
-    Optimized sampling of the permutohedron
+    Smart sampling of the permutohedron
     """
     def __init__(self,g,CoDual=False,actionsList=None,
                  orderLimit=50,
@@ -1616,8 +1616,8 @@ class RandomizedBachetRanking(PolarisedBachetRanking):
         import random as rd
         
         rd.seed(seed)
-        gcd = ~(-g)
-        triples = gcd.computeTransitivityDegree(ReturnIntransitiveTriples=True)
+        #gcd = ~(-g)
+        triples = g.computeTransitivityDegree(ReturnIntransitiveTriples=True)
         nt = len(triples)
         self.intransitiveTriples = triples
         if Debug:
@@ -1632,9 +1632,11 @@ class RandomizedBachetRanking(PolarisedBachetRanking):
             print(len(triples))
         t0 = time()
         al = [x for x in g.actions]
+        if Debug:
+            print(al)
         bestCorr = 0.0
         bestList = al
-        bestBa = PolarisedBachetRanking(gcd)
+        bestBa = PolarisedBachetRanking(g)
         for i in rdIndex:
             tr = triples[i]
             if Debug:
@@ -1643,21 +1645,26 @@ class RandomizedBachetRanking(PolarisedBachetRanking):
             yi = al.index(tr[1])
             al[xi] = tr[1]
             al[yi] = tr[0]
-            ba = PolarisedBachetRanking(gcd,actionsList=al)
+            if Debug:
+                print(al)
+            ba = PolarisedBachetRanking(g,actionsList=al)
             corr = g.computeRankingCorrelation(ba.bachetRanking)
             if corr['correlation'] > bestCorr:
                 bestList = al
                 bestCorr = corr['correlation']
-                bestBa = PolarisedBachetRanking(gcd,actionsList=bestList)
-            yi = al.index(tr[2])
-            #al[xi] = tr[1]
+                bestBa = PolarisedBachetRanking(g,actionsList=bestList)
+            yi = al.index(tr[1])
+            zi = al.index(tr[2])
             al[yi] = tr[2]
-            ba = PolarisedBachetRanking(gcd,actionsList=al)
+            al[zi] = tr[1]
+            if Debug:
+                print(al)
+            ba = PolarisedBachetRanking(g,actionsList=al)
             corr = g.computeRankingCorrelation(ba.bachetRanking)
             if corr['correlation'] > bestCorr:
                 bestList = al
                 bestCorr = corr['correlation']
-                bestBa = PolarisedBachetRanking(gcd,actionsList=bestList)
+                bestBa = PolarisedBachetRanking(g,actionsList=bestList)
         for att in bestBa.__dict__:
             self.__dict__[att] = bestBa.__dict__[att]
 
@@ -2721,8 +2728,9 @@ if __name__ == "__main__":
                                         corrba2['correlation'],
                                         corrnf['correlation']) )
     res.close()
-    ba4 = RandomizedBachetRanking(g,Debug=True,randomized=100)
+    ba4 = SmartBachetRanking(g,Debug=True,randomized=100)
     print(ba4)
+    print(ba4.correlation)
     
      
     print('*------------------*')
